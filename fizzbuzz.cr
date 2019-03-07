e2a2d80069a5aa4e4b2090567b50c8b6e445d791
“Brenda” <odonnell.brenda@gmail.com>
2019-03-07 07:35:11 +0000

    first fizzbuzz program

---
+++ fizzbuzz.cr
@@ -0,0 +1,19 @@
+class FizzBuzz
+
+puts "hello world"
+
+def self.play(num)
+  if (num % 3 == 0) && (num % 5 ==0)
+    puts "FizzBuzz"
+  elsif num % 3 == 0
+    puts "Fizz"
+  elsif num % 5 == 0
+    puts "Buzz"
+  else
+    puts num
+  end
+end
+
+end
+
+FizzBuzz.play(3)
